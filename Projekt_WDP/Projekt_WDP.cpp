c864265d8428c4af31849ab6270c6a78b5b2ee34
Albert Winiarski <albert.winiarski@student.uj.edu.pl>
2022-05-27 16:51:00 +0200

    Najaktualniejsza Wersja z plikami graficznymi

--- Projekt_WDP/Projekt_WDP.cpp
+++ Projekt_WDP/Projekt_WDP.cpp
@@ -31,7 +31,7 @@ int main()
 	ALLEGRO_BITMAP* dino = al_load_bitmap("dino_red.png");
 
 	al_start_timer(timer);
-	while (!koniec)   
+	while (!koniec)
 	{
 		al_wait_for_event(queue, &event);
 		switch (event.type) {
@@ -84,7 +84,7 @@ int main()
 	ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
 	ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
 
-	ALLEGRO_DISPLAY* disp = al_create_display(320, 200);
+	ALLEGRO_DISPLAY* disp = al_create_display(1900, 1060);
 
 	ALLEGRO_FONT* font = al_create_builtin_font();
 
@@ -103,12 +103,15 @@ int main()
 		return 1;
 	}
 
+	ALLEGRO_BITMAP* background = al_load_bitmap("background1.png");
 	ALLEGRO_BITMAP* jetpack = al_load_bitmap("jetpackman.png");
+
 	if (!jetpack)
-		{
+
+	{
 		printf("Nie udalo sie zaladowac zdjecia!\n");
 		return 1;
-		}
+	}
 
 
 	while (1) {
@@ -120,8 +123,11 @@ int main()
 		if (redraw && al_is_event_queue_empty(queue)) {
 			al_clear_to_color(al_map_rgb(0, 0, 0));
 			al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world");
+			al_draw_bitmap(background, 0, 0,0);
 			al_draw_bitmap(jetpack, 100, 100, 0);
+
 			al_flip_display();
+
 
 			redraw = false;
 		}
